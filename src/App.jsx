9f58c4c3ecff8709e365581eddd17891345b0cf3
Beyza Keser <beyzaaakeser@gmail.com>
2024-06-08 20:04:05 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Header from './components/Header';
+import Main from './pages/Main';
+import Card from './components/Card';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+        <Route path='/card' element={<Card/>}/>
+      </Routes>
     </BrowserRouter>
   );
 }
